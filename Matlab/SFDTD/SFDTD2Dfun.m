e9c1dcf5b79ccfd34aa3f3dcfb2085721da40138
SEDur <s.durbridge1@unimail.derby.ac.uk>
2017-03-01 23:04:34 +0000

    added new sfdtd addressing algorithm
    but its not qyuite performing as expected

--- Matlab/SFDTD/SFDTD2Dfun.m
+++ Matlab/SFDTD/SFDTD2Dfun.m
@@ -1,11 +1,11 @@
-function [p, ux, uy] = SFDTD2Dfun(p, pCx, pCy, ux, uy, uCx, uCy, Rx, Ry, ZL, ZR, ZT, ZB, pidxRow, pidxCol)
+function [p, ux, uy] = SFDTD2Dfun(p, pCx, pCy, ux, uy, uCx, uCy, Rx, Ry, ZL, ZR, ZT, ZB, idx)
 
     % update the non-boundary condition nodes for velocity
-%     ux(:, 2:end-1) = ux(:, 2:end-1) - uCx*(p(:, 2:end) - p(:, 1:end-1));
-%     uy(2:end-1, :) = uy(2:end-1, :) - uCy*(p(2:end, :) - p(1:end-1, :));
+    ux(:, 2:end-1) = ux(:, 2:end-1) - uCx*(p(:, 2:end) - p(:, 1:end-1));
+    uy(2:end-1, :) = uy(2:end-1, :) - uCy*(p(2:end, :) - p(1:end-1, :));
 
-    ux(pidxRow, pidxCol+1) = ux(pidxRow, pidxCol+1) - uCx*(p(pidxRow, pidxCol+1) - p(pidxRow, pidxCol));
-    uy(pidxRow+1, pidxCol) = uy(pidxRow+1, pidxCol) - uCy*(p(pidxRow+1, pidxCol) - p(pidxRow, pidxCol));
+%     ux(pidxRow, pidxCol+1) = ux(pidxRow, pidxCol+1) - uCx*(p(pidxRow, pidxCol+1) - p(pidxRow, pidxCol));
+%     uy(pidxRow+1, pidxCol) = uy(pidxRow+1, pidxCol) - uCy*(p(pidxRow+1, pidxCol) - p(pidxRow, pidxCol));
 
 
     % update the velocity at the right wall
@@ -23,6 +23,8 @@ function [p, ux, uy] = SFDTD2Dfun(p, pCx, pCy, ux, uy, uCx, uCy, Rx, Ry, ZL, ZR,
     uy(1, :) = ((Ry - ZB)/(Ry + ZB))*uy(1, :) - (2/(Ry + ZB))*p(1, :);
 
     % update the pressure at all nodes
-    p(pidxRow, pidxCol) = p(pidxRow, pidxCol) - pCx*(ux(pidxRow, pidxCol+1) - ux(pidxRow, pidxCol))...
-        - pCy*(uy(pidxRow+1, pidxCol) - uy(pidxRow, pidxCol));
+%     p(pidxRow, pidxCol) = p(pidxRow, pidxCol) - pCx*(ux(pidxRow, pidxCol+1) - ux(pidxRow, pidxCol))...
+%         - pCy*(uy(pidxRow+1, pidxCol) - uy(pidxRow, pidxCol));
+   p = p - pCx*(ux(:, 2:end) - ux(:, 1:end-1))...
+        - pCy*(uy(2:end, :) - uy(1:end-1, :));
 end
--- Matlab/SFDTD/SPARSEfun.m
+++ Matlab/SFDTD/SPARSEfun.m
@@ -1,16 +1,23 @@
-function [pidxRow, pidxCol, uxidx, uyidx] = SPARSEfun(p, thresholddB)
+function [idx] = SPARSEfun(p, thresholddB)
 
 threshold = 10^-12 * 10^(thresholddB/20);
 
-interpP = interp2(p);
+temp = p ./ threshold;
 
-[pidxRow, pidxCol] = find(abs(interpP)>threshold);
+temp = floor(temp);
 
-uxidx = 0;
-uyidx = 0;
+temp(temp > 1) = 1;
 
-if length(pidxCol) > 1
-surf(p(pidxRow, pidxCol))
-view(2);
+for i = 1 : size(temp, 1)
+temp2(i,:) = decimate(temp(i,:), 2);
 end
+for i = 1 : size(temp2, 2)
+temp3(:,i) = decimate(temp2(:,i), 2);
+end
+temp3 = ceil(temp3);
+
+temp4 = interp2(temp3);
+figure(2);
+mesh(temp4);
+idx = temp4;
 end
