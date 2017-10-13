4cdac2e7b0fb56bdd556e8eaf2b67fa8b414845b
Simon Durbridge <sdurbridge@bwgroup.com>
2017-10-13 12:12:54 +0100

    extraDocs

--- Matlab/findingSizes.m
+++ Matlab/findingSizes.m
@@ -1,16 +1,16 @@
-f = 500;
-c = 343;
-lambda = c/f;
-dx = lambda/6;
-spacex = 30 * 1/dx;
-spacey = 60 * 1/dx;
-spacez = 40 * 1/dx;
-volume = spacex * spacey * spacez;
-dt = 1 / c * sqrt((1/(dx^2))+(1/(dx^2))+(1/(dx^2)));
-numTimeSteps = 1/dt;
-myArray = ones(ceil(spacex), ceil(spacey), ceil(spacez));
-sizeOfArray = numel(myArray);
-numBytes = numel(myArray) * 4;
+% f = 500;
+% c = 343;
+% lambda = c/f;
+% dx = lambda/6;
+% spacex = 30 * 1/dx;
+% spacey = 60 * 1/dx;
+% spacez = 40 * 1/dx;
+% volume = spacex * spacey * spacez;
+% dt = 1 / c * sqrt((1/(dx^2))+(1/(dx^2))+(1/(dx^2)));
+% numTimeSteps = 1/dt;
+% myArray = ones(ceil(spacex), ceil(spacey), ceil(spacez));
+% sizeOfArray = numel(myArray);
+% numBytes = numel(myArray) * 4;
 %% 
 
 f = 1000 : 1000 : 20000;
@@ -25,6 +25,12 @@ dt = 1 / c .* (sqrt((1./(dx.^2))+(1./(dx.^2))+(1./(dx.^2))));
 numTimeSteps = 1./dt;
 myArraySize = ceil(spacex).* ceil(spacey) .* ceil(spacez);
 
-
+scatter(f / 1000, myArraySize, '*')
+set(gcf, 'Color', 'White');
+grid;
+xlabel('Frequency [kHz]')
+ylabel('Array Size [Elements]');
+ylabel('Matrix Size [Elements]');
+title('Matrix Size Vs Maximum Valid Simulation Data Frequency');
 % numberOfElements = numel(myArray);
 % numBytes = numel(myArray) * 4;
